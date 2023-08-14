28e14f20684d9d2e0ad8a2c2e81df12809f15ab5
KottaiRajAit <119566130+KottaiRajAit@users.noreply.github.com>
2023-08-14 22:39:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Middlewares
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use("/home", home);
--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios');
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+// router.get("/", async (req, res, next) => {
+//   return res.status(200).json({
+//     title: "Express Testing",
+//     message: "The app is working properly!",
+//   });
+// });
+
+
+
+
+//app.use(express.static(path.join(__dirname, "../client/build")))
+const RECAPTCHA_SECRET_KEY = '6LfDsqYnAAAAAMxr6c0OKKQ3ABvXoAQaHkuQvijM'; // Replace with your reCAPTCHA secret key
+
+router.post('/', async (req, res) => {
+  const data = req.body;
+  const captchaToken = data.captchaToken;
+  try {
+    // Verify reCAPTCHA token
+    const response = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${captchaToken}`
+    );
+        console.log('response', response);
+    if (response.data.success) {
+      console.log('reCAPTCHA verification successful');
+      console.log('Received data:', data);
+      // Process the data and send a response if needed
+      res.json({ message: 'Data received and reCAPTCHA verified successfully' });
+    } else {
+      console.log('reCAPTCHA verification failed');
+      res.status(400).json({ error: 'reCAPTCHA verification failed' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
